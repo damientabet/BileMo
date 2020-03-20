646472032747cc0bef3169c02f63c5f5f7e9aa7b
TABET Damien <tabetdamien@free.fr>
2020-03-20 16:18:43 +0100

    Create Client function
    - Create function
    - Update function
    - List view function
    - Detail views function
    - Delete function

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Service\ClientService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientController extends AbstractController
+{
+    /**
+     * @var ClientService
+     */
+    private $clientService;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(ClientService $clientService, EntityManagerInterface $entityManager)
+    {
+        $this->clientService = $clientService;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/client/{id}", name="client.show", methods={"GET"})
+     * @param Client $client
+     * @return Response
+     */
+    public function show(Client $client)
+    {
+        $data = $this->clientService->getClient($client);
+
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/clients/{page<\d+>?1}", name="client.index")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        $data = $this->clientService->getClientList($request);
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/clients/add", name="client.add", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request)
+    {
+        $this->clientService->addClient($request);
+        $data = [
+            'status' => 201,
+            'message' => 'Le client a bien été ajouté'
+        ];
+        return new JsonResponse($data, 201);
+    }
+
+    /**
+     * @Route("/client/{id}", name="client.update", methods={"PUT"})
+     * @param Request $request
+     * @param Client $client
+     * @return JsonResponse|Response
+     */
+    public function update(Request $request, Client $client)
+    {
+        $this->clientService->updateClient($request, $client);
+        $data = [
+            'status' => 200,
+            'message' => 'Le client a bien été mis à jour'
+        ];
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/client/{id}", name="client.delete", methods={"DELETE"})
+     * @param Client $client
+     * @return Response
+     */
+    public function delete(Client $client)
+    {
+        $this->entityManager->remove($client);
+        $this->entityManager->flush();
+
+        $data = [
+            'status' => 204,
+            'message' => 'Le client a bien été supprimé'
+        ];
+        return new JsonResponse($data);
+    }
+}
---
+++ src/Service/ClientService.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Client;
+use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * Class ClientService
+ * @package App\Service
+ */
+class ClientService
+{
+    /**
+     * @var ClientRepository
+     */
+    private $clientRepository;
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+    private $limit;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    private $validator;
+
+    public function __construct(ClientRepository $clientRepository, SerializerInterface $serializer, EntityManagerInterface $entityManager, ValidatorInterface $validator, $limit)
+    {
+        $this->clientRepository = $clientRepository;
+        $this->serializer = $serializer;
+        $this->entityManager = $entityManager;
+        $this->validator = $validator;
+        $this->limit = $limit;
+    }
+
+    public function getClientList(Request $request)
+    {
+        $page = $request->query->get('page');
+        if(is_null($page) || $page < 1) {
+            $page = 1;
+        }
+        $phones = $this->clientRepository->findAllClients($page,$this->limit);
+        return $this->serializer->serialize($phones, 'json', [
+            'groups' => ['list']
+        ]);
+    }
+
+    public function getClient(Client $client)
+    {
+        $client = $this->clientRepository->find($client->getId());
+        return $this->serializer->serialize($client, 'json', [
+            'groups' => ['show']
+        ]);
+    }
+
+    public function addClient(Request $request)
+    {
+        $client = $this->serializer->deserialize($request->getContent(), Client::class, 'json');
+        $errors = $this->validator->validate($client);
+        if(count($errors)) {
+            $errors = $this->serializer->serialize($errors, 'json');
+            return new Response($errors, 500, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        $this->entityManager->persist($client);
+        $this->entityManager->flush();
+    }
+
+    public function updateClient(Request $request, Client $client)
+    {
+        $clientUpdate = $this->clientRepository->findOneBy(['id' => $client->getId()]);
+        $data = json_decode($request->getContent());
+        foreach ($data as $key => $value){
+            if($key && !empty($value)) {
+                $name = $key;
+                $setter = 'set'.$name;
+                $clientUpdate->$setter($value);
+            }
+        }
+        $errors = $this->validator->validate($clientUpdate);
+        if(count($errors)) {
+            $errors = $this->serializer->serialize($errors, 'json');
+            return new Response($errors, 500, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        $this->entityManager->flush();
+    }
+}
