0a8211ca3ecc3933c1df2b7267c8006c6feebab1
TABET Damien <tabetdamien@free.fr>
2020-03-20 10:23:16 +0100

    Add construct function into phone controller

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -4,32 +4,53 @@ namespace App\Controller;
 
 use App\Entity\Phone;
 use App\Repository\PhoneRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PhoneController extends AbstractController
 {
     private $limit;
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+    /**
+     * @var PhoneRepository
+     */
+    private $phoneRepository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
 
-    public function __construct($limit)
+    public function __construct($limit, SerializerInterface $serializer, EntityManagerInterface $entityManager, PhoneRepository $phoneRepository, ValidatorInterface $validator)
     {
         $this->limit = $limit;
+        $this->serializer = $serializer;
+        $this->phoneRepository = $phoneRepository;
+        $this->entityManager = $entityManager;
+        $this->validator = $validator;
     }
 
     /**
      * @Route("/phone/{id}", name="phone.show", methods={"GET"})
      * @param Phone $phone
-     * @param PhoneRepository $phoneRepository
-     * @param SerializerInterface $serializer
      * @return Response
      */
-    public function show(Phone $phone, PhoneRepository $phoneRepository, SerializerInterface $serializer)
+    public function show(Phone $phone)
     {
-        $phone = $phoneRepository->find($phone->getId());
-        $data = $serializer->serialize($phone, 'json', [
+        $phone = $this->phoneRepository->find($phone->getId());
+        $data = $this->serializer->serialize($phone, 'json', [
             'groups' => ['show']
         ]);
 
@@ -41,18 +62,16 @@ class PhoneController extends AbstractController
     /**
      * @Route("/phones/{page<\d+>?1}", name="phone.index", methods={"GET"})
      * @param Request $request
-     * @param PhoneRepository $phoneRepository
-     * @param SerializerInterface $serializer
      * @return Response
      */
-    public function index(Request $request, PhoneRepository $phoneRepository, SerializerInterface $serializer)
+    public function index(Request $request)
     {
         $page = $request->query->get('page');
         if(is_null($page) || $page < 1) {
             $page = 1;
         }
-        $phones = $phoneRepository->findAllPhones($page,$this->limit);
-        $data = $serializer->serialize($phones, 'json', [
+        $phones = $this->phoneRepository->findAllPhones($page,$this->limit);
+        $data = $this->serializer->serialize($phones, 'json', [
             'groups' => ['list']
         ]);
 
