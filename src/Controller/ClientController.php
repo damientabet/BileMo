0fd18ad294a5a2fda45e13bab56e8da673ef8b6d
TABET Damien <tabetdamien@free.fr>
2020-04-24 10:33:27 +0200

    Refactoring "ClientController" code

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,43 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\Client;
-use App\Service\ClientService;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ClientController extends AbstractController
+class ClientController extends Controller
 {
-    /**
-     * @var ClientService
-     */
-    private $clientService;
-    /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
-
-    public function __construct(ClientService $clientService, EntityManagerInterface $entityManager)
-    {
-        $this->clientService = $clientService;
-        $this->entityManager = $entityManager;
-    }
+    protected $serviceName = "clientService";
 
     /**
      * @Route("/client/{id}", name="client.show", methods={"GET"})
      * @param Client $client
      * @return Response
      */
-    public function show(Client $client)
+    public function displayClient(Client $client)
     {
-        $data = $this->clientService->getClient($client);
-
-        return new Response($data, 200, [
-            'Content-Type' => 'application/json'
-        ]);
+        return $this->show($client);
     }
 
     /**
@@ -47,12 +27,9 @@ class ClientController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    public function index(Request $request)
+    public function displayAllClients(Request $request)
     {
-        $data = $this->clientService->getClientList($request);
-        return new Response($data, 200, [
-            'Content-Type' => 'application/json'
-        ]);
+        return $this->index($request);
     }
 
     /**
@@ -60,14 +37,9 @@ class ClientController extends AbstractController
      * @param Request $request
      * @return JsonResponse
      */
-    public function new(Request $request)
+    public function addClient(Request $request)
     {
-        $this->clientService->addClient($request);
-        $data = [
-            'status' => 201,
-            'message' => 'Le client a bien été ajouté'
-        ];
-        return new JsonResponse($data, 201);
+        return $this->new($request);
     }
 
     /**
@@ -76,14 +48,9 @@ class ClientController extends AbstractController
      * @param Client $client
      * @return JsonResponse|Response
      */
-    public function update(Request $request, Client $client)
+    public function updateClient(Request $request, Client $client)
     {
-        $this->clientService->updateClient($request, $client);
-        $data = [
-            'status' => 200,
-            'message' => 'Le client a bien été mis à jour'
-        ];
-        return new JsonResponse($data);
+        return $this->update($request, $client);
     }
 
     /**
@@ -91,15 +58,8 @@ class ClientController extends AbstractController
      * @param Client $client
      * @return Response
      */
-    public function delete(Client $client)
+    public function deleteClient(Client $client)
     {
-        $this->entityManager->remove($client);
-        $this->entityManager->flush();
-
-        $data = [
-            'status' => 204,
-            'message' => 'Le client a bien été supprimé'
-        ];
-        return new JsonResponse($data);
+        return $this->delete($client);
     }
 }
