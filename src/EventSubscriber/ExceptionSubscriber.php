aad3fcfc2c17c3f253594ab85eab369c6db991c2
TABET Damien <tabetdamien@free.fr>
2020-05-15 17:30:08 +0200

    Create new exceptions

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -6,7 +6,9 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
@@ -17,6 +19,22 @@ class ExceptionSubscriber implements EventSubscriberInterface
             $data = [
                 'status' => $exception->getStatusCode(),
                 'message' => 'Resource not found'
+            ];
+
+            $response = new JsonResponse($data);
+            $event->setResponse($response);
+        } elseif ($exception->getStatusCode() === 401) {
+            $data = [
+                'status' => $exception->getStatusCode(),
+                'message' => $exception->getMessage()
+            ];
+
+            $response = new JsonResponse($data);
+            $event->setResponse($response);
+        } elseif ($exception instanceof AccessDeniedHttpException) {
+            $data = [
+                'status' => $exception->getStatusCode(),
+                'message' => $exception->getMessage()
             ];
 
             $response = new JsonResponse($data);
